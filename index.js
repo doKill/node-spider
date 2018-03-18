946f0507409bec931f4bb82c03fffefc11de620c
doKill <funccn@gmail.com>
2018-03-18 16:56:03 +0800

    :bug: forEmoj

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var app = express();
 var ejs = require('ejs');
 
 
-app.engine('html',ejs.__express);
-app.set('view engine','html');
+app.engine('html', ejs.__express);
+app.set('view engine', 'html');
 app.set('views', './views'); //设置模板路径
 
 
